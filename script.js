dd793232ab9a6e86878176fd091cceb222dfea12
SamerIQ56X <94875411+SamerIQ56X@users.noreply.github.com>
2025-01-14 00:27:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.si = window.si || function () { (window.siq = window.siq || []).push(arguments); };
-window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
 
 function decodeBase64(encoded) {
     return atob(encoded);
